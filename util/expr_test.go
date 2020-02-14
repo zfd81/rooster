a221249ea72ebd9f42ba4a56f948555d2819595e
zfd81 <zfd81@189.cn>
2020-02-14 15:36:55 +0800

    代码整理

--- util/expr_test.go
+++ util/expr_test.go
@@ -1,22 +1,10 @@
 package util
 
 import (
-	"reflect"
 	"testing"
-	"time"
 
 	"github.com/spf13/cast"
 )
-
-type User struct {
-	Id                int
-	Name              string
-	Password          string
-	Number            string
-	Department_id     int
-	Created_date      time.Time
-	Lastmodified_date time.Time
-}
 
 func TestExprParsing(t *testing.T) {
 	m := map[string]interface{}{
@@ -29,24 +17,4 @@ func TestExprParsing(t *testing.T) {
 	}
 	s := "aa" + cast.ToString(v)
 	t.Log(s)
-
-	//mm := make(map[string]string)
-	//var mm map[string]string
-	var us User = User{}
-	vv := reflect.ValueOf(us)
-	if vv.Kind() != reflect.Ptr {
-		t.Error("err")
-	}
-
-	if vv.IsNil() {
-		t.Error("==========")
-	}
-	//t.Log(len(mm))
-	//aa(&mm)
-	//t.Log(len(mm))
-}
-
-func aa(p *map[string]string) {
-	(*p)["aa"] = "aa"
-	(*p)["bb"] = "bb"
 }
